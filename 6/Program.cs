c85f951d30096b7eb750a78c3ffd75a010021b41
Eduard Abdrashitov <a.eduardmail@gmail.com>
2023-02-10 20:20:51 +0300

    HW 1 Seminar

---
+++ 6/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Enter number: ");
+int number = int.Parse(Console.ReadLine());
+if (number / 2 == 0)
+    Console.Write(number + " Yes");
+else
+    Console.Write(number + " No");
---
+++ 8/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Enter number: ");
+int n = int.Parse(Console.ReadLine());
+    
+for (int i = 1; i <= n; i++)
+    if (i % 2 == 0)
+        Console.WriteLine(i);
