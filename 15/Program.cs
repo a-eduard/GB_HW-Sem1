b22f9a230d34fb93f6b1c7106908b3cf8e30d1af
Eduard Abdrashitov <a.eduardmail@gmail.com>
2023-02-14 18:03:00 +0300

    2 Seminar

--- 15/Program.cs
+++ 15/Program.cs
@@ -2,7 +2,7 @@
 Console.Write("Enter number: ");
 int number = int.Parse(Console.ReadLine());
 
-if (number == 1 || number == 2 || number == 3 || number == 4 || number == 5)
+if (number <= 5)
 {
     Console.WriteLine("No");
     return;
