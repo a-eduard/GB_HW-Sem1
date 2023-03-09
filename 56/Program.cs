1156de79872f4b9aa7ac6b89829aba506ead702f
Eduard Abdrashitov <a.eduardmail@gmail.com>
2023-03-09 12:45:41 +0300

    Добавлены задачи 54, 56, 58, 60, 61, 62

---
+++ 56/Program.cs
@@ -0,0 +1,73 @@
+﻿/*Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7
+Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка*/
+
+using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите количество строк массива: ");
+int rows = int.Parse(ReadLine());
+
+Write("Введите количество столбцов массива: ");
+int columns = int.Parse(ReadLine());
+
+int[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
+
+
+int LineMin= 0;
+int SumLine = ElementSum(array, 0);
+for (int i = 1; i < array.GetLength(0); i++)
+{
+  int tempSumLine = ElementSum(array, i);
+  if (SumLine > tempSumLine)
+  {
+    SumLine = tempSumLine;
+    LineMin = i;
+  }
+}
+
+Console.WriteLine($"\n{LineMin + 1} - строкa с наименьшей суммой ({SumLine}) элементов ");
+
+int[,] GetArray(int m, int n, int min, int max)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(min, max + 1);
+        }
+
+    }
+    return result;
+}
+
+
+int ElementSum(int[,] array, int i)
+{
+  int SumLine = array[i, 0];
+  for (int j = 1; j < array.GetLength(1); j++)
+  {
+    SumLine += array[i, j];
+  }
+  return SumLine;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Write($"{array[i, j]} ");
+        }
+        WriteLine();
+    }
+}
---
+++ 58/Program.cs
@@ -0,0 +1,78 @@
+﻿/*Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+Например, даны 2 матрицы:
+2 4 | 3 4
+3 2 | 3 3
+Результирующая матрица будет:
+18 20
+15 18*/
+
+using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите количество строк массива 1 массива: ");
+int rows1 = int.Parse(ReadLine());
+
+Write("Введите количество столбцов массива 1 массива: ");
+int columns1 = int.Parse(ReadLine());
+
+Write("Введите количество строк массива 2 массива: ");
+int rows2 = int.Parse(ReadLine());
+
+Write("Введите количество столбцов массива 2 массива: ");
+int columns2 = int.Parse(ReadLine());
+
+int[,] array1 = GetArray(rows1, columns1, 0, 10);
+int[,] array2 = GetArray(rows2, columns2, 0, 10);
+int[,] arrayResult = new int[rows1,columns1];
+PrintArray(array1);
+WriteLine();
+PrintArray(array2);
+WriteLine();
+ResultMatrix(array1, array2, arrayResult);
+PrintArray(arrayResult);
+
+
+void ResultMatrix(int[,] array1, int[,] array2, int[,] arrayResult)
+{
+  for (int i = 0; i < arrayResult.GetLength(0); i++)
+  {
+    for (int j = 0; j < arrayResult.GetLength(1); j++)
+    {
+      int sum = 0;
+      for (int k = 0; k < array1.GetLength(1); k++)
+      {
+        sum += array1[i,k] * array2[k,j];
+      }
+      arrayResult[i,j] = sum;
+    }
+  }
+}
+
+int[,] GetArray(int m, int n, int min, int max)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(min, max + 1);
+        }
+
+    }
+    return result;
+}
+
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Write($"{array[i, j]} ");
+        }
+        WriteLine();
+    }
+}
